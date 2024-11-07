2a0a55b5fc0c73d046db675c032fb585d57d1fce
MFactor1 <mingsqu@gmail.com>
2024-11-07 01:55:26 -0500

    Added second page

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
-from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.properties import ListProperty, StringProperty
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.label import Label
+from kivy.core.window import Window
 
 BUTTON_BASE = [50/255, 50/255, 50/255, 0.8]
 INI = dict()
@@ -141,23 +142,21 @@ class Initializer():
         INI[Toks.FADE_TM] = 1.5
         INI[Toks.DEBUG] = False
 
+class CmdWnd(GridLayout):
+    initializer = Initializer()
+    initializer.readIni()
 
-class CommandScreen(Screen):
-    def on_enter(self, *args):
-        initializer = Initializer()
-        initializer.readIni()
+    kp = ObjectProperty(None)
+    yes = ObjectProperty(None)
+    no = ObjectProperty(None)
+    custom = ObjectProperty(None)
 
-        kp = self.ids.key_press
-        yes = self.ids.yes_btn
-        no = self.ids.no_btn
-        custom = self.ids.custom_btn
-
-        if ini_error:
-            header_text = StringProperty(ini_error_msg)
-            font_size = 20
-        else:
-            header_text = StringProperty(INI[Toks.HEADER_TXT])
-            font_size = 50
+    if ini_error:
+        header_text = StringProperty(ini_error_msg)
+        font_size = 20
+    else:
+        header_text = StringProperty(INI[Toks.HEADER_TXT])
+        font_size = 50
 
     def on_key_down(self, window, key, scancode, codepoint, modifiers):
         try:
@@ -179,13 +178,117 @@ class CommandScreen(Screen):
         self.no.update()
         self.custom.update()
 
+class CommandScreen(Screen):
+    cmd_wnd = ObjectProperty(None)
+
+    def update(self, dt):
+        self.cmd_wnd.update(dt)
+
+    def on_key_down(self, window, key, scancode, codepoint, modifiers):
+        self.cmd_wnd.on_key_down(window, key, scancode, codepoint, modifiers)
+
+class Key(Widget):
+    text = StringProperty('')
+
+class KeyWnd(GridLayout):
+    def __init__(self, **kwargs):
+        super(KeyWnd, self).__init__(**kwargs)
+
+        self.text = 'test text testsdf fkjsdl dl sdlk test test text nba'
+        self.key_objs = dict()
+        self.key_maps = dict()
+
+        for _ in range(4):
+            self.add_widget(Widget(size_hint_x=None,size_hint_y=None,width=0))
+
+        self.title = Label(font_size=50, halign='center', text='Keyboard')
+        self.title.center_x = self.center_x + 100
+        self.add_widget(self.title)
+
+        for _ in range(5):
+            self.add_widget(Widget(size_hint_x=None,size_hint_y=None,width=0))
+
+        self.add_widget(Label(font_size=50, text=self.text, halign='left'))
+        """
+        self.add_widget(Label(
+                            font_size=20,
+                            halign='left',
+                            text_size=(self.width * 10, self.height),
+                            text=self.text))
+        """
+
+        for _ in range(9):
+            self.add_widget(Widget(size_hint_x=None,size_hint_y=None,width=0))
+
+        keys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+                'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '\u232B',
+                'z', 'x', 'c', 'v', 'b', 'n', 'm', '.', '\u2423', 'CLEAR']
+
+        for i, disp_char in enumerate(keys):
+            if disp_char == '\u232B':
+                char = '\b'
+            elif disp_char == '\u2423':
+                char = ' '
+            elif disp_char == 'CLEAR':
+                char = 'CLR'
+            else:
+                char = disp_char
+
+            pos = f"{i - 10 * (int(i / 10))},{int(i / 10)}"
+            self.key_objs[pos] = Key(text=disp_char)
+            self.key_maps[pos] = char
+            self.add_widget(self.key_objs[pos])
+
+    def update(self, dt):
+        pass
+
+    def on_key_down(self, window, key, scancode, codepoint, modifiers):
+        pass
+
+class KeyboardScreen(Screen):
+    key_wnd = ObjectProperty(None)
+
+    def update(self, dt):
+        self.key_wnd.update(dt)
+
+    def on_key_down(self, window, key, scancode, codepoint, modifiers):
+        self.key_wnd.on_key_down(window, key, scancode, codepoint, modifiers)
+
+class MyScreenManager(ScreenManager):
+    cmd_scr = None
+    key_scr = None
+
+    def update(self, dt):
+        if self.current == 'CommandScreen' and self.cmd_scr is not None:
+            self.cmd_scr.update(dt)
+        elif self.current == 'KeyboardScreen' and self.key_scr is not None:
+            self.key_scr.update(dt)
+
+    def on_key_down(self, window, key, scancode, codepoint, modifiers):
+        print(f"Pressed {key}")
+        if key == 32:
+            if self.current == 'CommandScreen':
+                self.current = 'KeyboardScreen'
+            else:
+                self.current = 'CommandScreen'
+        else:
+            if self.current == 'CommandScreen' and self.cmd_scr is not None:
+                self.cmd_scr.on_key_down(window, key, scancode, codepoint, modifiers)
+            elif self.current == 'KeyboardScreen' and self.key_scr is not None:
+                self.key_scr.on_key_down(window, key, scancode, codepoint, modifiers)
+
 class CommApp(App):
     def build(self):
-        screen_manager = ScreenManager()
-        wnd = CommandScreen()
-        Window.bind(on_key_down=wnd.on_key_down)
-        Clock.schedule_interval(wnd.update, 1.0/30)
-        return wnd
+        sm = MyScreenManager()
+        cmd_scr = CommandScreen(name='CommandScreen')
+        key_scr = KeyboardScreen(name='KeyboardScreen')
+        sm.add_widget(cmd_scr)
+        sm.add_widget(key_scr)
+        sm.cmd_scr = cmd_scr
+        sm.key_scr = key_scr
+        Window.bind(on_key_down=sm.on_key_down)
+        Clock.schedule_interval(sm.update, 1.0/30)
+        return sm
 
 if __name__ == "__main__":
     CommApp().run()
